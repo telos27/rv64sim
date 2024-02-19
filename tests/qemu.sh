3e8279495d569574f74d79c1e1a2c03e4ea60c26
telos27 <149587669+telos27@users.noreply.github.com>
2024-02-19 15:10:19 -0800

    Added FreeRTOS.txt and qemu.sh

---
+++ tests/qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-riscv32 -nographic -machine virt -net none -chardev stdio,id=con,mux=on -serial chardev:con -mon chardev=con,mode=readline -bios none -smp 1 -kernel ./build/gcc/output/RTOSDemo.elf
